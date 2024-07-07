09f9b46d253cca5e34c33e92d7606291a6dad177
Tatiana <frrrolova@MacBook-Pro-Tatiana.local>
2024-07-07 23:33:44 +0200

    feat: fix styles for error boundary

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -19,15 +19,18 @@ class ErrorBoundary extends React.Component<PropsWithChildren, { hasError: boole
     if (this.state.hasError) {
       return (
         <div className={styles.eBoundary}>
-          <h1>Something went wrong.</h1>
-          <div>
-            <button
-              onClick={() => {
-                this.setState((state) => ({ ...state, hasError: false }));
-              }}
-            >
-              Reset
-            </button>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Something went wrong.</h1>
+            <div>
+              <button
+                className={styles.btn}
+                onClick={() => {
+                  this.setState((state) => ({ ...state, hasError: false }));
+                }}
+              >
+                Reset
+              </button>
+            </div>
           </div>
         </div>
       );
